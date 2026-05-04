3288b63eb521d7309e731fa8ec64df4be44923b1
Yaku Guzman <yakumgc12@gmail.com>
2026-05-04 14:11:26 -0500

    Add Kotlin Symbol Processing (KSP) plugin to the project
    - Added KSP version to `libs.versions.toml`.
    - Defined the `kotlin-ksp` plugin in the version catalog.
    - Applied the KSP plugin in the root `build.gradle.kts` and the app-level `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.ksp)
 
 }
 
